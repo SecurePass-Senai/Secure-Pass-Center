2ef21622e7c52145fdfcd5dbebf9ecd9733e0c23
Priscila-Rodrigues <41398579866@info.local>
2023-08-31 20:24:13 -0300

    atualizao 31/08

---
+++ src/components/Botoes/index.tsx
@@ -0,0 +1,31 @@
+import "./style.css"
+
+export default function Botoes() {
+    // aqui vai js
+    return (
+        <>
+        <div className="menu">
+          <nav className="menu_ss">
+  <div className="btn_cadastrar">
+    <a href="../cadastro/cadastro.html">Cadastrar</a>
+  </div>
+  <br />
+  <div className="btn_cadastrar">
+    <a href="../acoes/Home/acoes.html">Ações</a>
+  </div>
+  <br />
+  <div className="btn_cadastrar">
+    <a href="../perfil/perfil.html">Perfil</a>
+  </div>
+  <br />
+  <div className="btn_cadastrar">
+    <a href="../BiometriaSur5/index.html">Sair</a>
+  </div>
+  <br />
+</nav>
+</div>
+          
+        </>
+    )
+}
+
